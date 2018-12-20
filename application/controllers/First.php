3aa7578252539d9c6b6ec745e4f571eb68b30ad9
Harsha Thota <harshathota12345@gmail.com>
2018-12-20 18:30:06 +0530

    Controller created
    first controller

---
+++ application/controllers/First.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class First extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file First.php */
+/* Location: ./application/controllers/First.php */
